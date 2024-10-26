d095c65ba47123841f7c0f96b71a24e8dbe7a5f4
Vaibhav252002 <vaibhavjondhale25@gmail.com>
2024-10-26 19:03:46 +0530

    Message

---
+++ script.js
@@ -0,0 +1,13 @@
+let rollNumber = 2002;
+//it convert number into the string
+console.log(rollNumber.toString().length);
+console.log(rollNumber.toFixed(2));
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+const pureNumber = -53543;
+console.log(Math.abs(pureNumber));
+
+console.log(Math.random());
